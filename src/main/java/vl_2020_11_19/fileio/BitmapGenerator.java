63dc2e65ae5050f41c952cd575c121cbc2adbff2
Philipp Molitor <philipp@molitor.cloud>
2020-11-19 23:11:12 +0100

    add WIP Bitmap generator (incorrect image data bytes)

---
+++ src/main/java/vl_2020_11_19/fileio/BitmapGenerator.java
@@ -0,0 +1,123 @@
+package vl_2020_11_19.fileio;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.InvalidParameterException;
+import java.util.Arrays;
+
+public class BitmapGenerator {
+    private int sizeX;
+    private int sizeY;
+    private byte[] fileHeaderData;
+    private byte[] bitmapHeaderData;
+    private byte[] imageData;
+
+    public BitmapGenerator(int sizeX, int sizeY) throws Exception {
+        if (sizeX <= 0 || sizeY <= 0) {
+            throw new Exception("bitmap must be at least 1x1 pixels.");
+        }
+
+        // size properties
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        imageData = new byte[sizeX * sizeY * 3];
+        Arrays.fill(imageData, (byte) 0);
+
+        // headers
+        fileHeaderData = getFileHeader();
+        bitmapHeaderData = getBitmapHeader();
+    }
+
+    public void setPixelRgb(int x, int y, byte r, byte g, byte b) {
+        checkImageBounds(x, y);
+
+        int baseIndex = sizeX * y + x;
+        imageData[baseIndex + 0] = b;
+        imageData[baseIndex + 1] = g;
+        imageData[baseIndex + 2] = r;
+    }
+
+    public void setPixelGrayscale(int x, int y, byte k) {
+        checkImageBounds(x, y);
+
+        int baseIndex = sizeY * y + x;
+        imageData[baseIndex] = k;
+        imageData[baseIndex + (byte) 1] = k;
+        imageData[baseIndex + (byte) 2] = k;
+    }
+
+    public int getSizeX() {
+        return sizeX;
+    }
+
+    public int getSizeY() {
+        return sizeY;
+    }
+
+    public byte[] getImageAsBytes() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        // concatenate output bytes (headers + image data)
+        try {
+            outputStream.write(getFileHeader());
+            outputStream.write(getBitmapHeader());
+            outputStream.write(imageData);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+        return outputStream.toByteArray();
+    }
+
+    private void checkImageBounds(int x, int y) {
+        if (x < 0 || x >= sizeX || y < 0 || y >= sizeY) {
+            throw new InvalidParameterException("invalid pixel, please choose a pixel within the bitmap size.");
+        }
+    }
+
+    private byte[] getFileHeader() {
+        int size = 14 + 40 + (sizeX * sizeY * 3);
+
+        return new byte[]{
+                // magic header
+                'B', 'M',
+                // file size (reverse order)
+                (byte) size, (byte) (size >>> 8), (byte) (size >>> 16), (byte) (size >>> 24),
+                // reserved
+                0, 0, 0, 0,
+                // data offset
+                54, 0, 0, 0,
+        };
+    }
+
+    private byte[] getBitmapHeader() {
+        int size = sizeX * sizeY * 3;
+
+        return new byte[]{
+                // header size
+                40, 0, 0, 0,
+                // bitmap width
+                (byte) sizeX, (byte) (sizeX >>> 8), (byte) (sizeX >>> 16), (byte) (sizeX >>> 24),
+                // bitmap height (positive: bottom-up)
+                (byte) sizeY, (byte) (sizeY >>> 8), (byte) (sizeY >>> 16), (byte) (sizeY >>> 24),
+                // planes (legacy)
+                1, 0,
+                // 24bit color-depth
+                24, 0,
+                // compression (disabled)
+                0, 0, 0, 0,
+                // image data size
+                (byte) size, (byte) (size >>> 8), (byte) (size >>> 16), (byte) (size >>> 24),
+                // pixels in X dimension per meter
+                0, 0, 0, 0,
+                // pixels in Y dimension per meter
+                0, 0, 0, 0,
+                // used color table
+                0, 0, 0, 0,
+                // important colors
+                0, 0, 0, 0,
+        };
+    }
+
+}
---
+++ src/main/java/vl_2020_11_19/fileio/BitmapGeneratorMain.java
@@ -0,0 +1,36 @@
+package vl_2020_11_19.fileio;
+
+import java.io.FileWriter;
+
+public class BitmapGeneratorMain {
+    public static void main(String[] args) {
+        BitmapGenerator bg = null;
+        int sizeX = 4;
+        int sizeY = 4;
+
+        // try to create an image handle
+        try {
+            bg = new BitmapGenerator(sizeX, sizeY);
+        } catch (Exception ex) {
+            System.out.println("unable to create new image!");
+            ex.printStackTrace();
+            System.exit(1);
+        }
+
+        for (int x = 0; x < sizeX; x++)
+            for (int y = 0; y < sizeX; y++)
+                bg.setPixelRgb(x, y, (byte) 0xff, (byte) 0xff, (byte) 0xff);
+
+        // save file
+        try {
+            FileWriter fw = new FileWriter("output_generator.bmp");
+            fw.write(new String(bg.getImageAsBytes(), "UTF-8").toCharArray());
+            fw.flush();
+            fw.close();
+            System.out.println("image was saved!");
+        } catch (Exception ex) {
+            System.out.println("failed to save image:");
+            ex.printStackTrace();
+        }
+    }
+}
